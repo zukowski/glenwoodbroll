f2747f05dc8caac06c95e6dcc0c3884e72c1c611
Chris Boertien <chris.boertien@gmail.com>
2011-05-27 23:14:42 -0600

    Basic implementation of layout
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   def render_404
     render :file => "#{Rails.root.to_s}/public/404.html", :status => 404
   end
+
+  def current_video_collection
+    video_collection = VideoCollection.find_by_id(session[:vc_id])
+    if video_collection.nil? && current_user
+      video_collection = VideoCollection.create(:user => current_user)
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def category_options
+    [['foo',1],['bar',2]]
+  end
+
+  def render_shadow_box(id, content)
+    render :partial => 'shared/shadow_box', :locals => {:id => id, :content => content}
+  end
 end
