1d675ac2a9c4474ab23cf38247aa05431c4b6bfb
Chris Boertien <chris.boertien@gmail.com>
2011-06-01 17:01:24 -0600

    Buttons for adding videos to a collection
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def render_shadow_box(id, content)
     render :partial => 'shared/shadow_box', :locals => {:id => id, :content => content}
   end
+
+  def link_to_populate(video)
+    button_to('Add To Reel', populate_collection_path(current_collection, :video_id => video), :method => :post, :class => 'button')
+  end
 end
